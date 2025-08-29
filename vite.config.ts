e996224ada8876313b75c69d47b81061b854abf3
olafgeppert-droid <olaf.geppert@googlemail.com>
2025-08-30 01:42:05 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Wichtig für GitHub Pages
+  // ganz wichtig für GitHub Pages:
   base: '/Google.AI.Studio_v1.23_inkl.Wappen/',
 })
